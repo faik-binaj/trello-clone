c8e2231b9fc369a7b5e31218b806464e98776973
faikbinaj <faik.binaj@gmail.com>
2024-01-13 17:12:43 +0100

    add support to search todos

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,14 @@ import React from "react";
 import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import useBoardStore from "@/store/BoardStore";
 
 const Header = () => {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -24,6 +30,8 @@ const Header = () => {
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
             <input
               type="text"
+              value={searchString}
+              onChange={(event) => setSearchString(event.target.value)}
               className="flex-1 outline-none p-2"
               placeholder="Search..."
             />
